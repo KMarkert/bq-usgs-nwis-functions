390a39755a11fc5bfe443a0a11791f4c533721c9
Kel Markert <kmarkert@google.com>
2023-05-20 23:25:29 -0500

    adding functionality to request multiple sites in one query

--- src/usgs_nwis/main.py
+++ src/usgs_nwis/main.py
@@ -11,23 +11,24 @@ def get_streamflow(request):
     try:
       request_json = request.get_json(silent=True)
       calls = request_json['calls']
+      n_calls = len(calls)
 
       # unpack the call data
       stations, dates = list(zip(*calls))
 
-      # get the first station from call
-      # this assumes that all rows have the same station
-      station = stations[0]
+      # get the unique stations in call
+      stations_set = list(set(stations))
 
       # extract the min and max date range for the call
       # don't want to loop through all dates but make one call to nwis
       min_date, max_date = min(dates), max(dates)
-      print(min_date, max_date)
+      print(f'Station set: {stations_set}\nMin date: {min_date}\nMax date: {max_date}')
       
       # request the data from NWIS API for the time range
+      # and multiple stations if in call
       # this is more efficient than looping through each
       gauge = nwis.get_record(
-        sites=station, 
+        sites=stations_set, 
         service='dv', 
         start=min_date, 
         end=max_date, 
@@ -37,12 +38,32 @@ def get_streamflow(request):
       # convert cfs to cms
       gauge['streamflow'] = gauge['00060_Mean'] * 0.02832
 
-      # make sure to only get the data for the times requested
-      # this insures the n rows are same for returned data
-      gauge_req = gauge.loc[gauge.index.strftime('%Y-%m-%d').isin(dates)]
+      if len(stations_set) > 1:
+        # if there are multiple stations in the call
+        # get the station and dates index for selecting data
+        ix = pd.MultiIndex.from_arrays(
+          [stations, dates], 
+          names = ('site_no','datetime')
+        )
+
+      else:
+        # if only one station
+        # get the dates index for selecting data
+        ix = dates
+
+      # check if there is too little or too much data returned by NWIS
+      if n_calls > gauge.shape[0]:
+          gauge_req = gauge.reindex(ix)
+      elif n_calls < gauge.shape[0]:
+        gauge_req = gauge.loc[ix]
+      else:
+        gauge_req = gauge
 
       # convert to list to pass to the response
-      replies = gauge_req['streamflow'].tolist()
+      # and set poor data to -999999 value
+      replies = gauge_req['streamflow'].where(
+        gauge_req['streamflow']>=0, -999999
+      ).fillna(-999999).tolist()
 
       return json.dumps({'replies': replies})
 
